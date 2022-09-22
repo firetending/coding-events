8fb73cc2a14da01af02d06f2828e8c8c6ed793ed
firetending <alan.miller.375@gmail.com>
2022-09-22 02:03:33 -0400

    form handler method

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +13,15 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
+
     //lives at /events
-    @GetMapping()
+    @GetMapping
     public String events(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Ice Cream Social");
-        events.add("Cocktail Mixer");
-        events.add("Hack-A-Thon");
-        events.add("Brunch");
+//        events.add("Ice Cream Social");
+//        events.add("Cocktail Mixer");
+//        events.add("Hack-A-Thon");
+//        events.add("Brunch");
         model.addAttribute("events",events);
         return "events/index";
     }
@@ -27,5 +30,12 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model) {
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:"; //redirects to root of controller
     }
 }
